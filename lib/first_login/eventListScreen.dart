127bafc4c87af631bc12649b2c6247c01ca1c7ec
Petr <pan.petrsula@gmail.com>
2024-03-14 13:02:07 +0100

    sgsdf

--- lib/first_login/eventListScreen.dart
+++ lib/first_login/eventListScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:my_app_2/models/importantEvent.dart';
 
 import '../models/partner.dart';
@@ -31,9 +32,21 @@ class _EventListScreenState extends State<EventListScreen> {
       body: ListView.builder(
         itemCount: events.length,
         itemBuilder: (context, index) {
+          final event = events[index];
+          Color tileColor = _getColorForType(event.type);
+
           return ListTile(
             title: Text(events[index].name),
-            subtitle: Text('${events[index].date.toString()}'),
+            subtitle: Text(_formatDate(events[index].date)),
+            tileColor: tileColor,
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                setState(() {
+                  _removeEvent(index);
+                });
+              }
+            )
           );
         },
       ),
@@ -41,6 +54,7 @@ class _EventListScreenState extends State<EventListScreen> {
   }
 
   List<ShowEvent> fillEvents() {
+    addBaseEvents();
     if (widget.partnerData.birthDay != null) {
       events.add(birthday());
     }
@@ -49,9 +63,6 @@ class _EventListScreenState extends State<EventListScreen> {
     }
     if (widget.partnerData.anniversary!= null){
       events.add(anniversary());
-    }
-    if (widget.partnerData.importantEvents != null){
-      events.addAll(importantEvents());
     }
     if (widget.partnerData.importantEvents != null){
       events.addAll(importantEvents());
@@ -88,7 +99,7 @@ class _EventListScreenState extends State<EventListScreen> {
       ShowEvent showEvent = ShowEvent(
         name: i.eventName,
         date: i.eventDate,
-        type: 0, // Set the appropriate type value
+        type: 3, // Set the appropriate type value
       );
       events.add(showEvent);
     }
@@ -96,5 +107,44 @@ class _EventListScreenState extends State<EventListScreen> {
     return events;
 
   }
+  String _formatDate(DateTime date) {
+    return DateFormat('dd-MM').format(date);
+  }
+  void _removeEvent(int index) {
+    setState(() {
+      events.removeAt(index);
+    });
+  }
 
+  Color _getColorForType(int type) {
+    switch (type) {
+      case 0:
+        return Colors.blue;
+      case 1:
+        return Colors.green;
+      case 2:
+        return Colors.orange;
+      case 3:
+        return Colors.purple;
+      case 4:
+        return Colors.pink;
+      default:
+        return Colors.grey; // Default color for unknown types
+    }
+  }
+
+  void addBaseEvents() {
+    ShowEvent valentDay = ShowEvent(
+      name: "Valentine's day",
+      date: DateTime(DateTime.now().year, 2, 14),
+      type: 4, // Set the appropriate type value
+    );
+    events.add(valentDay);
+    ShowEvent IWD = ShowEvent(
+        name: "International Women's Day ",
+        date: DateTime(DateTime.now().year, 3, 8),
+        type: 4);
+    events.add(IWD);
+
+  }
 }
